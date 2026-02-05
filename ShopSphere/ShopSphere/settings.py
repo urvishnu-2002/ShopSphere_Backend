c1af17a28ffe2484e7174a556b03e00af262bda7
uppalapatinandini <uppalapatinandini16@gmail.com>
2026-02-05 18:10:33 +0530

    vendor login and registration

--- ShopSphere/ShopSphere/settings.py
+++ ShopSphere/ShopSphere/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_recaptcha',
+    'vendor',
+    
 ]
 
 MIDDLEWARE = [
@@ -115,3 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Silence reCAPTCHA test key warning in development
+SILENCED_SYSTEM_CHECKS = ['django_recaptcha.recaptcha_test_key_error']
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/6.0/ref/settings/#default-auto-field
