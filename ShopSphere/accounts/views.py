2302544e2e3ee52b773e4bd8526a62e382a7ebdc
urvishnu-2002 <i.m.vishnuvardhan2002@gmail.com>
2026-02-09 11:32:37 +0530

    resolve

--- ShopSphere/accounts/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render, redirect
-from django.contrib import messages
-
-def signup_choice(request):
-    """Choose account type"""
-    return render(request, 'auth/signup_choice.html')
